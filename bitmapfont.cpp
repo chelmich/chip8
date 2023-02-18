4b26e426b958447898449fe6607f183eff76c8d8
Charles Helmich <charlie.helmich@gmail.com>
2023-02-18 13:16:52 -0700

    Clean up BitmapFont class somewhat

--- bitmapfont.cpp
+++ bitmapfont.cpp
@@ -1,22 +1,38 @@
 #include "bitmapfont.hpp"
 
+#include <SDL2/SDL_image.h>
+
+BitmapFont::BitmapFont(SDL_Renderer *renderer, char const *filename, int char_width, int char_height)
+    : char_width(char_width), char_height(char_height), m_renderer(renderer)
+{
+    m_texture = IMG_LoadTexture(renderer, filename);
+    if (m_texture == nullptr) {
+        // TODO: throw if loading fails
+    }
+
+    SDL_QueryTexture(m_texture, NULL, NULL, &m_tex_width, &m_tex_height);
+}
+
+BitmapFont::~BitmapFont() {
+    SDL_DestroyTexture(m_texture);
+}
+
 void BitmapFont::drawChar(char c, int x, int y, int scale) {
-    int cols = tex_width / char_width;
-    int rows = tex_height / char_height;
+    int cols = m_tex_width / char_width;
+    int rows = m_tex_height / char_height;
 
     int index = c - 32; // font bitmap skips first 32 ascii chars
-    SDL_Rect src_rect = {
-        char_width * (index % cols),
-        char_height * (index / cols),
-        char_width, char_height};
+    int col = index % cols;
+    int row = index / cols;
+    if (row > rows) return; // TODO: signal an error?
 
+    SDL_Rect src_rect = {char_width * col, char_height * row, char_width, char_height};
     SDL_Rect dst_rect = {x, y, char_width * scale, char_height * scale};
-
     SDL_RenderCopy(m_renderer, m_texture, &src_rect, &dst_rect);
 }
 
 void BitmapFont::drawStr(char const* str, int x, int y, int scale) {
-    int len = strlen(str);
+    size_t len = strlen(str);
     for (int i = 0; i < len; i++) {
         drawChar(str[i], x + (char_width * scale * i), y, scale);
     }
--- bitmapfont.hpp
+++ bitmapfont.hpp
@@ -1,24 +1,12 @@
 #pragma once
 
 #include <SDL2/SDL_render.h>
-#include <SDL2/SDL_image.h>
 
 class BitmapFont {
 public:
-    BitmapFont(SDL_Renderer* renderer, char const* filename, int width, int height):
-        char_width(width), char_height(height), m_renderer(renderer)
-    {
-        m_texture = IMG_LoadTexture(renderer, filename);
-        if (m_texture == nullptr) {
-            // TODO: throw if loading fails
-        }
+    BitmapFont(SDL_Renderer *renderer, char const *filename, int char_width, int char_height);
 
-        SDL_QueryTexture(m_texture, NULL, NULL, &tex_width, &tex_height);
-    }
-
-    ~BitmapFont() {
-        SDL_DestroyTexture(m_texture);
-    }
+    ~BitmapFont();
 
     // Font pixel dimensions
     const int char_width, char_height;
@@ -30,5 +18,5 @@ private:
     SDL_Renderer* m_renderer;
 
     SDL_Texture* m_texture;
-    int tex_width, tex_height;
+    int m_tex_width, m_tex_height;
 };
